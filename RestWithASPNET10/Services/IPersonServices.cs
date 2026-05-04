0cb471bff0156da572c66e5a353e86197ed70f31
Rafael Menezes <rafa.rcm@hotmail.com>
2026-05-04 15:00:18 -0300

    implementando-ipersonservices

---
+++ RestWithASPNET10/Services/IPersonServices.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNET10.Model;
+
+namespace RestWithASPNET10.Services
+{
+    public interface IPersonServices
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
