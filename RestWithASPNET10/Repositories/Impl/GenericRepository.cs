bff14939f741296b5e4f01355a5af68716a4690a
Rafael Menezes <rafa.rcm@hotmail.com>
2026-07-04 14:31:10 -0300

    finalizando-implementacao-repositorio-generico

--- RestWithASPNET10/Repositories/Impl/GenericRepository.cs
+++ RestWithASPNET10/Repositories/Impl/GenericRepository.cs
@@ -1,36 +1,60 @@
-﻿using RestWithASPNET10.Model.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithASPNET10.Model.Base;
+using RestWithASPNET10.Model.Context;
 
 namespace RestWithASPNET10.Repositories.Impl
 {
     public class GenericRepository<T> : IRepository<T> where T : BaseEntity
     {
+        private MSSQLContext _context;
+        private DbSet<T> _dataset;
+        public GenericRepository(MSSQLContext context)
+        {
+            _context = context;
+            _dataset = _context.Set<T>();
+        }
         public List<T> FindAll()
         {
-            throw new NotImplementedException();
+            return _dataset.ToList();
         }
 
         public T FindById(long id)
         {
-            throw new NotImplementedException();
+            return _dataset.Find(id);
         }
 
         public T Create(T item)
         {
-            throw new NotImplementedException();
+            _context.Add(item);
+            _context.SaveChanges();
+            return item;
         }
         public T Update(T item)
         {
-            throw new NotImplementedException();
+            var itemBD = _dataset.Find(item.Id);
+            if (itemBD == null)
+            {
+                return null;
+            }
+            _context.Entry(itemBD).CurrentValues.SetValues(item);
+            _context.SaveChanges();
+            return item;
         }
 
         public void Delete(long id)
         {
-            throw new NotImplementedException();
+            var item = _dataset.Find(id);
+            if (item == null)
+            {
+                return;
+            }
+            _context.Remove(item);
+            _context.SaveChanges();
         }
 
         public bool Exists(long id)
         {
-            throw new NotImplementedException();
+            return _dataset.Any(p => p.Id.Equals(id));
         }
     }
 }
