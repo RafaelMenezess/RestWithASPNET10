08502a9551e6e15137de9dfbf6c6c67221b0c136
Rafael Menezes <rafa.rcm@hotmail.com>
2026-07-23 09:30:39 -0300

    data-jsonconverter

--- RestWithASPNET10/Data/DTO/V2/PersonDTO.cs
+++ RestWithASPNET10/Data/DTO/V2/PersonDTO.cs
@@ -1,18 +1,20 @@
-﻿using System.Text.Json.Serialization;
+﻿using RestWithASPNET10.JsonSerializers;
+using System.Text.Json.Serialization;
 
 namespace RestWithASPNET10.Data.DTO.V2
 {
     public class PersonDTO
     {
-        [JsonPropertyName("code")]
+        //[JsonPropertyName("code")]
         public long Id { get; set; }
-        [JsonPropertyName("first_name")]
+        //[JsonPropertyName("first_name")]
         public string FirstName { get; set; }
-        [JsonPropertyName("last_name")]
+        //[JsonPropertyName("last_name")]
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Gender { get; set; }
-        [JsonPropertyName("birth_day")]
+        //[JsonPropertyName("birth_day")]
+        [JsonConverter(typeof(DaterSerializer))]
         public DateTime? BirthDay { get; set; }
     }
 }
