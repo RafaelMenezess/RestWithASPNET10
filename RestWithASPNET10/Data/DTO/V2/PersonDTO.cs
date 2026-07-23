d05136f2415bfd9faf645c080792097fc0103487
Rafael Menezes <rafa.rcm@hotmail.com>
2026-07-23 09:20:11 -0300

    custon-json

--- RestWithASPNET10/Data/DTO/V2/PersonDTO.cs
+++ RestWithASPNET10/Data/DTO/V2/PersonDTO.cs
@@ -1,12 +1,18 @@
-﻿namespace RestWithASPNET10.Data.DTO.V2
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNET10.Data.DTO.V2
 {
     public class PersonDTO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
+        [JsonPropertyName("first_name")]
         public string FirstName { get; set; }
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Gender { get; set; }
+        [JsonPropertyName("birth_day")]
         public DateTime? BirthDay { get; set; }
     }
 }
